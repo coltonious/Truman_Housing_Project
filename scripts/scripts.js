e1c9c3eacfb31d0aaa7e8fce5f6f20f61c54e748
coltonious <therandomguy900@gmail.com>
2024-10-02 21:50:24 -0500

    some basic stuff for css, javascript, and home

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+//hamburger menu open
+function openMenu()
+{
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block")
+    {
+        x.style.display = "none";
+    }
+    else
+    {
+        x.style.display = "block"
+    }
+}
